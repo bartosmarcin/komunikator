892900f15484e0be1448ef6df9b1b96511854e30
bartosmarcin <bartosmarcin@gmail.com>
2013-05-06 15:10:50 +0200

    zmiana nazwy service w manifescie(NewMessageListener)

--- src/com/example/komunikator/NewMessageListener.java
+++ src/com/example/komunikator/NewMessageListener.java
@@ -4,16 +4,32 @@ import android.app.IntentService;
 import android.content.Intent;
 
 public class NewMessageListener extends IntentService{
-	int intervalInForeground;
-	int intervalInBackground;
+	long intervalInForeground;
+	long intervalInBackground;
+	Connection connection;
 		
 	public NewMessageListener(){
 		super("NewMessageListener");
+		connection = new Connection();
+		intervalInForeground = 10000;
+		intervalInBackground = 300000;
 	}
 
 	@Override
 	protected void onHandleIntent(Intent arg0) {
-		//TODO add code
+		// naive implementation
+		//TODO find more efficient solution(like AlarmManager)
+		//TODO implement msgs handling
+		long start = System.currentTimeMillis() + intervalInForeground;
+		while(true){
+			if(System.currentTimeMillis() > start){
+				if ( connection.hasNewMessages("marcin") ){
+					String messages = connection.getNewMessages("marcin");		
+
+				}
+				start = System.currentTimeMillis() + intervalInForeground;				
+			}			
+		}
 		
 	}
 
