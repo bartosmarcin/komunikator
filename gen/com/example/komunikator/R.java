6b279cd9f533cd331efb7bfdc4cc3bf5a464957d
bartosmarcin <bartosmarcin@gmail.com>
2013-05-08 01:52:46 +0200

    naprawiam po rafale

--- gen/com/example/komunikator/R.java
+++ gen/com/example/komunikator/R.java
@@ -24,14 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int From=0x7f080004;
-        public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080001;
-        public static final int description=0x7f080005;
-        public static final int editText1=0x7f080002;
-        public static final int icon=0x7f080003;
-        public static final int listView1=0x7f080000;
-        public static final int time=0x7f080006;
+        public static final int From=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int description=0x7f080003;
+        public static final int icon=0x7f080001;
+        public static final int time=0x7f080004;
+        public static final int txt=0x7f080000;
     }
     public static final class layout {
         public static final int activity_conversation=0x7f030000;
