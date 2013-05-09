7de12101b97e4c50796a493b267bb7c8b04b65f6
RafalZawadzki <rafal.zawadzki3@gmail.com>
2013-05-09 23:51:01 +0200

    conversation static

--- src/com/example/komunikator/Conversation.java
+++ src/com/example/komunikator/Conversation.java
@@ -5,28 +5,28 @@ import java.util.ArrayList;
 public class Conversation {
 	 public static ArrayList<Message> details = new ArrayList<Message>();;
 	
-	public void add(Message m){
+	public static void add(Message m){
 		details.add(m);
 	}
 	
-	public Message getMostRecent(){
+	public static Message getMostRecent(){
 		int last = details.size() - 1;
 		return details.get(last);
 	}
 	
-	public Message getMessage(int index){
+	public static Message getMessage(int index){
 		return details.get(index);
 	}
 	   
-	public void deleteMessage(int index){
+	public static void deleteMessage(int index){
 		details.remove(index);
 	}
 	
-	public void deleteMessage(Message message){
+	public static void deleteMessage(Message message){
 		details.remove(message);
 	}
 	
-	public void deleteAll(){
+	public static void deleteAll(){
 		details=null;
 	}
 }
