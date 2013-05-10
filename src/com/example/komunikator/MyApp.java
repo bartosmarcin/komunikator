c70f4c181feedb8c5ec8030c71439752fd629a2e
bartosmarcin <bartosmarcin@gmail.com>
2013-05-10 16:51:34 +0200

    Dodanie klasy informujacej, czy appka dziala w tle

---
+++ src/com/example/komunikator/MyApp.java
@@ -0,0 +1,20 @@
+package com.example.komunikator;
+
+import android.app.Application;
+
+public class MyApp extends Application{
+	
+	private static boolean activityVisible=true;
+	
+	public static boolean isInForeground(){
+		return activityVisible;
+	}
+	
+	public static void activityResumed() {
+	    activityVisible = true;
+	  }
+
+	  public static void activityPaused() {
+	    activityVisible = false;
+	  }
+}
