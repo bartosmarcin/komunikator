4d71cce8014965bad00e4ef108ea2862ba92ee23
bartosmarcin <bartosmarcin@gmail.com>
2013-05-13 16:14:32 +0200

    Nie wywala sie po wznowieniu

--- src/com/example/komunikator/ConversationActivity.java
+++ src/com/example/komunikator/ConversationActivity.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.view.KeyEvent;
-import android.view.WindowManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -32,6 +31,7 @@ public class ConversationActivity extends Activity {
 		super.onResume();
 		MyApp.activityResumed();
 		registerReceiver(broadcastReciever, intentFilter);
+		ad.notifyDataSetChanged();
 	}
 
 	@Override
