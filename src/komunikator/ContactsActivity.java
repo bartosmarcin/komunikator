1c467947570d73cf38f4d1feff1f5da8e2a1f695
Rafal1 <rafal.zawadzki3@gmail.com>
2014-09-16 23:27:49 +0200

    improve drawer

--- src/komunikator/ContactsActivity.java
+++ src/komunikator/ContactsActivity.java
@@ -1,8 +1,11 @@
 package komunikator;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,24 +22,55 @@ public class ContactsActivity extends Activity {
     private String[] drawerListViewItems;
     private ListView drawerListView;
     private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.contacts);
+//    public static ConversationAdapter ad;
 
-            // get list items from strings.xml
-            drawerListViewItems = getResources().getStringArray(R.array.items);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts);
 
-            // get ListView defined in contacts.xml
-            drawerListView = (ListView) findViewById(R.id.left_drawer);
+        // get list items from strings.xml
+        drawerListViewItems = getResources().getStringArray(R.array.items);
 
-            // Set the adapter for the list view
-            drawerListView.setAdapter(new ArrayAdapter<String>(this,
-                    R.layout.drawer_listview_item, drawerListViewItems));
+        // get ListView defined in contacts.xml
+        drawerListView = (ListView) findViewById(R.id.left_drawer);
 
-//            drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Set the adapter for the list view
+        drawerListView.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_listview_item, drawerListViewItems));
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_launcher,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        );
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // call ActionBarDrawerToggle.onOptionsItemSelected(), if it returns true
+        // then it has handled the app icon touch event
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
+        return super.onOptionsItemSelected(item);
+    }
 
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
