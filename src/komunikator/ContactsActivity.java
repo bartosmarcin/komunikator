e9f0bdbf5c83587e4b3baeeba4e96f834bae5422
Rafal1 <rafal.zawadzki3@gmail.com>
2014-09-24 14:54:34 +0200

    contact's Activity action bar (with searchView)

--- src/komunikator/ContactsActivity.java
+++ src/komunikator/ContactsActivity.java
@@ -1,15 +1,22 @@
 package komunikator;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.view.ActionProvider;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,8 +30,6 @@ public class ContactsActivity extends Activity {
     private ListView drawerListView;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
-
-//    public static ConversationAdapter ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,10 +77,26 @@ public class ContactsActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.contacts_action_bar, menu);
+//        menu.findItem(R.id.action_compose).setActionProvider(android.view.MenuItem, ActionProvider);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
     private class DrawerItemClickListener implements ListView.OnItemClickListener {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
             Toast.makeText(ContactsActivity.this, ((TextView) view).getText(), Toast.LENGTH_LONG).show();
             drawerLayout.closeDrawer(drawerListView);
         }
     }
+
 }
