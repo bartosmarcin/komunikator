61a313df8ffc78d3c9fa06b1bfa6fcc0cc9ef37b
bartosmarcin <bartosmarcin@gmail.com>
2014-09-27 00:58:42 +0200

    Moved webService field to parent class (WebServiceActivity)

--- src/komunikator/WebServiceActivity.java
+++ src/komunikator/WebServiceActivity.java
@@ -1,13 +1,39 @@
 package komunikator;
 
+import com.example.komunikator.R;
+
+import WebService.WebService;
 import WebService.WebServiceResponse;
 import android.app.Activity;
+import android.view.View;
+import android.widget.TextView;
 
 public abstract class WebServiceActivity extends Activity {
+	
+	protected WebService webService;
 
 	public abstract void onRequestSuccess(WebServiceResponse response);
 	
 	public abstract void onRequestFailure();
+	
+	@Override
+	public void onStart(){
+		super.onStart();
+		webService = new WebService(this);
+	}	
+
+	protected void showError(final String message) {
+		final TextView errorTextView = (TextView) findViewById(R.id.error_text_view);
+		errorTextView.setText(message);
+		errorTextView.setVisibility(View.VISIBLE);
+		errorTextView.setAlpha(0);
+		errorTextView.animate().alpha(1).withLayer();
+		errorTextView.postDelayed(new Runnable() {
+			public void run() {
+				errorTextView.animate().alpha(0);
+			}
+		}, 3000);
+	}
 	
 	
 }
