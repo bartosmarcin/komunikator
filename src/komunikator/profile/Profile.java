438f3a3a47e6d851976a845ff54bc383a228dc45
bartosmarcin <bartosmarcin@gmail.com>
2014-09-29 09:17:02 +0200

    Profile model class

---
+++ src/komunikator/profile/Profile.java
@@ -0,0 +1,52 @@
+package komunikator.profile;
+
+import komunikator.utils.DatabaseObject;
+
+public class Profile implements DatabaseObject{
+	
+	public Long id;
+	public String email;
+	public String firstName;
+	public String lastName;
+	public String nickName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getNickName() {
+		return nickName;
+	}
+	
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+}
