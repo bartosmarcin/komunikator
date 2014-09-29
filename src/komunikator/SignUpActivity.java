04041f89da130a293e2e9e90fc9c00b5cc9cda2d
bartosmarcin <bartosmarcin@gmail.com>
2014-09-29 17:06:18 +0200

    minor changes

--- src/komunikator/SignUpActivity.java
+++ src/komunikator/SignUpActivity.java
@@ -1,21 +1,22 @@
 package komunikator;
 
+import java.util.List;
+
+import komunikator.profile.Profile;
+import komunikator.profile.ProfileDAO;
+import komunikator.profile.ProfileEditActivity;
+import WebService.WebServiceRequest;
+import WebService.WebServiceResponse;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.example.komunikator.R;
-
-import WebService.WebService;
-import WebService.WebServiceRequest;
-import WebService.WebServiceResponse;
 
 public class SignUpActivity extends WebServiceActivity {
 
